ed24e5faed5888a92c34b52f71b4ad47f3411a0b
Huy Nguyen <huuhuybn@gmail.com>
2017-04-17 17:38:46 +0700

    abc

---
+++ app/src/main/java/vn/tonish/hozo/activity/other/ProfileActivity.java
@@ -0,0 +1,32 @@
+package vn.tonish.hozo.activity.other;
+
+import vn.tonish.hozo.R;
+import vn.tonish.hozo.activity.BaseActivity;
+
+/**
+ * Created by huyquynh on 4/12/17.
+ */
+
+public class ProfileActivity extends BaseActivity {
+
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_profile;
+    }
+
+    @Override
+    protected void initView() {
+        setBackButton();
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void resumeData() {
+
+    }
+}
