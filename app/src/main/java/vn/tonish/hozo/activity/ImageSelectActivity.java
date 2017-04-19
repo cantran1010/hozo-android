0883a5692dfa2ad7d25d0ba2002076ca4606ad45
long bui <longbd@tonish.vn>
2017-04-19 17:53:09 +0700

    layout + map

---
+++ app/src/main/java/vn/tonish/hozo/activity/ImageSelectActivity.java
@@ -0,0 +1,163 @@
+package vn.tonish.hozo.activity;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.GridView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import vn.tonish.hozo.R;
+import vn.tonish.hozo.adapter.ImageSelectAdapter;
+import vn.tonish.hozo.common.Constants;
+import vn.tonish.hozo.model.Image;
+
+/**
+ * Created by ADMIN on 4/19/2017.
+ */
+
+public class ImageSelectActivity extends BaseActivity implements View.OnClickListener {
+
+    private String album;
+    private GridView grImage;
+    private ImageSelectAdapter imageSelectAdapter;
+    private ArrayList<Image> images;
+    private final String[] projection = new String[]{MediaStore.Images.Media._ID, MediaStore.Images.Media.DISPLAY_NAME, MediaStore.Images.Media.DATA};
+    private RelativeLayout layoutBack;
+    private TextView tvDone,tvAlbumName;
+
+    @Override
+    protected int getLayout() {
+        return R.layout.image_select_activity;
+    }
+
+    @Override
+    protected void initView() {
+        layoutBack = (RelativeLayout) findViewById(R.id.layout_back);
+        layoutBack.setOnClickListener(this);
+
+        tvDone = (TextView) findViewById(R.id.tv_done);
+        tvDone.setOnClickListener(this);
+
+        tvAlbumName = (TextView) findViewById(R.id.tv_album_name);
+
+        grImage = (GridView) findViewById(R.id.gr_image);
+    }
+
+    @Override
+    protected void initData() {
+        Intent intent = getIntent();
+        if (intent == null) {
+            finish();
+        }
+        album = intent.getStringExtra(Constants.INTENT_EXTRA_ALBUM);
+        tvAlbumName.setText(album);
+
+        getImage();
+    }
+
+    @Override
+    protected void resumeData() {
+
+    }
+
+    private void getImage() {
+        AsyncTask<Void, Void, Void> imageTask = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+
+                File file;
+
+                Cursor cursor = getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection,
+                        MediaStore.Images.Media.BUCKET_DISPLAY_NAME + " =?", new String[]{album}, MediaStore.Images.Media.DATE_ADDED);
+                if (cursor == null) {
+                    return null;
+                }
+
+            /*
+            In case this runnable is executed to onChange calling loadImages,
+            using countSelected variable can result in a race condition. To avoid that,
+            tempCountSelected keeps track of number of selected images. On handling
+            FETCH_COMPLETED message, countSelected is assigned value of tempCountSelected.
+             */
+                int tempCountSelected = 0;
+                ArrayList<Image> temp = new ArrayList<>(cursor.getCount());
+                if (cursor.moveToLast()) {
+                    do {
+                        if (Thread.interrupted()) {
+                            return null;
+                        }
+
+                        long id = cursor.getLong(cursor.getColumnIndex(projection[0]));
+                        String name = cursor.getString(cursor.getColumnIndex(projection[1]));
+                        String path = cursor.getString(cursor.getColumnIndex(projection[2]));
+//                        boolean isSelected = selectedImages.contains(id);
+//                        if (isSelected) {
+//                            tempCountSelected++;
+//                        }
+
+                        file = new File(path);
+                        if (file.exists()) {
+                            temp.add(new Image(id, name, path, false, false));
+                        }
+
+                    } while (cursor.moveToPrevious());
+                }
+                cursor.close();
+
+                if (images == null) {
+                    images = new ArrayList<>();
+                }
+                images.clear();
+                images.addAll(temp);
+
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                imageSelectAdapter = new ImageSelectAdapter(ImageSelectActivity.this, images);
+                grImage.setAdapter(imageSelectAdapter);
+            }
+        };
+        imageTask.execute();
+    }
+
+    private void sendIntent() {
+        Intent intent = new Intent();
+        intent.putParcelableArrayListExtra(Constants.INTENT_EXTRA_IMAGES, getSelectedImage());
+        setResult(Constants.RESPONSE_CODE_PICKIMAGE, intent);
+        finish();
+    }
+
+    private ArrayList<Image> getSelectedImage() {
+        ArrayList<Image> selectedImages = new ArrayList<>();
+        for (int i = 0, l = images.size(); i < l; i++) {
+            if (images.get(i).isSelected) {
+                selectedImages.add(images.get(i));
+            }
+        }
+        return selectedImages;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.layout_back:
+                finish();
+                break;
+
+            case R.id.tv_done:
+                sendIntent();
+                break;
+
+        }
+    }
+}
---
+++ app/src/main/java/vn/tonish/hozo/adapter/ImageAdapter.java
@@ -0,0 +1,90 @@
+package vn.tonish.hozo.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+import vn.tonish.hozo.R;
+import vn.tonish.hozo.model.Image;
+import vn.tonish.hozo.utils.LogUtils;
+
+/**
+ * Created by ADMIN on 4/19/2017.
+ */
+
+public class ImageAdapter extends ArrayAdapter<Image> {
+    private static final String TAG = ImageAdapter.class.getName();
+
+    public interface ImageAdapterListener{
+        public void onImageAdapterListener();
+    }
+
+    private ImageAdapterListener imageAdapterListener;
+
+    public ImageAdapterListener getImageAdapterListener() {
+        return imageAdapterListener;
+    }
+
+    public void setImageAdapterListener(ImageAdapterListener imageAdapterListener) {
+        this.imageAdapterListener = imageAdapterListener;
+    }
+
+    public ImageAdapter(Context _context, ArrayList<Image> address) {
+        super(_context, R.layout.item_image, address);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final Image item = getItem(position);
+
+        LogUtils.d(TAG, "getView , item : " + item.toString());
+
+        final ViewHolder holder;
+        if (convertView == null) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getContext());
+            convertView = layoutInflater.inflate(R.layout.item_image, parent, false);
+            holder = new ViewHolder();
+            holder.imgImage = (ImageView) convertView.findViewById(R.id.img_image);
+            holder.imgAdd = (ImageView) convertView.findViewById(R.id.img_add);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        if(item.isAdd){
+            holder.imgAdd.setVisibility(View.VISIBLE);
+            holder.imgAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                  if(imageAdapterListener != null) imageAdapterListener.onImageAdapterListener();
+                }
+            });
+        }else{
+            holder.imgAdd.setVisibility(View.GONE);
+            holder.imgAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
+
+        Glide.with(getContext())
+                .load(item.getPath())
+                .centerCrop().into(holder.imgImage);
+
+        return convertView;
+    }
+
+    public static class ViewHolder {
+        ImageView imgImage, imgAdd;
+    }
+}
