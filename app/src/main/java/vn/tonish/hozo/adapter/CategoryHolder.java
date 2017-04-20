8d930b4f3bff14ebac4c8869b6ddd6361620d2ad
long bui <longbd@tonish.vn>
2017-04-20 17:37:22 +0700

    post a task layout

--- app/src/main/java/vn/tonish/hozo/adapter/CategoryHolder.java
+++ app/src/main/java/vn/tonish/hozo/adapter/CategoryHolder.java
@@ -3,6 +3,7 @@ package vn.tonish.hozo.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import vn.tonish.hozo.R;
@@ -16,6 +17,7 @@ import vn.tonish.hozo.model.Category;
 
 public class CategoryHolder extends BaseHolder {
     public final TextView tvName;
+    public ImageView imgPresent;
     public final View.OnClickListener onClickListener;
     public View view;
     public Category category;
@@ -24,6 +26,7 @@ public class CategoryHolder extends BaseHolder {
         super(itemView, context);
         view = itemView;
         tvName = (TextView) itemView.findViewById(R.id.tv_name);
+        imgPresent = (ImageView) itemView.findViewById(R.id.img_present);
         onClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
