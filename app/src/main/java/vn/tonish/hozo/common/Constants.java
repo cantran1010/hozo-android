a886bd58f9fa019bece4135f10d91dc7b48c67a4
cantran85 <cantv@tonish.vn>
2017-04-22 08:32:49 +0700

    commit 22-1

--- app/src/main/java/vn/tonish/hozo/common/Constants.java
+++ app/src/main/java/vn/tonish/hozo/common/Constants.java
@@ -16,11 +16,6 @@ public class Constants {
     public static final int RESPONSE_CODE_ADDRESS = 1234;
     public static final String EXTRA_ADDRESS = "extra_address";
     public static final String EXTRA_ONLY_IMAGE = "extra_only_image";
-
-    public static final int REQUEST_CODE_ADDRESS = 2345;
-    public static final int RESPONSE_CODE_ADDRESS = 1234;
-    public static final String EXTRA_ADDRESS = "extra_address";
-    public static final String EXTRA_ONLY_IMAGE = "extra_only_image";
     public static boolean DEBUG = true;
     public static final int SPLASH_TIME = 1500;
     public static final String DB_NAME = "hozo";
