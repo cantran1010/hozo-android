0a4cd2cceaf286399b532094ff20c5129774c46a
cantran85 <cantv@tonish.vn>
2017-04-23 11:12:16 +0700

    commit-23-2

--- app/src/main/java/vn/tonish/hozo/activity/LoginScreen.java
+++ app/src/main/java/vn/tonish/hozo/activity/LoginScreen.java
@@ -87,7 +87,7 @@ public class LoginScreen extends BaseActivity implements View.OnClickListener {
                     }
                     tvContinue.setEnabled(false);
                     if (CheckErrorEditText(edtPhone.getText().toString().trim())) {
-                        error = "So dien thoai khong dung dinh dang";
+                        error = getResources().getString(R.string.login_erro_phone);
                         edtPhone.startAnimation(mLoadAnimation);
                         edtPhone.setError(error);
                     }
@@ -257,7 +257,6 @@ public class LoginScreen extends BaseActivity implements View.OnClickListener {
                         showExtendView(OTP_VIEW);
                     } else if (jsonResponse.getInt("code") == 1) {
                         Toast.makeText(context, "Mobile is empty", Toast.LENGTH_SHORT).show();
-
                     } else {
                         Toast.makeText(context, "Error", Toast.LENGTH_SHORT).show();
                     }
