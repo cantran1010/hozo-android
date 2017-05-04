8404d011f65566a48ba7ad583b8125fb805cc1ea
cantran85 <cantv@tonish.vn>
2017-05-04 18:37:07 +0700

    update_search_ui

--- app/src/main/java/vn/tonish/hozo/fragment/BrowseTaskFragment.java
+++ app/src/main/java/vn/tonish/hozo/fragment/BrowseTaskFragment.java
@@ -1,35 +1,17 @@
 package vn.tonish.hozo.fragment;
 
-import android.os.Handler;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TimerTask;
+import android.view.View;
+import android.widget.ImageView;
 
 import vn.tonish.hozo.R;
-import vn.tonish.hozo.adapter.WorkAdapter;
-import vn.tonish.hozo.model.Work;
-import vn.tonish.hozo.network.NetworkUtils;
-import vn.tonish.hozo.utils.EndlessRecyclerViewScrollListener;
 
 /**
  * Created by Admin on 4/4/2017.
  * Edited by huyquynh on 19/4/2017
  */
 
-public class BrowseTaskFragment extends BaseFragment implements NetworkUtils.NetworkListener {
-
-    private static double lat = 21.000030;
-    private static double lon = 105.837400;
-
-    private RecyclerView lvList;
-    private WorkAdapter workAdapter;
-    private LinearLayoutManager lvManager;
-    private List<Work> workList;
+public class BrowseTaskFragment extends BaseFragment implements View.OnClickListener {
+    private ImageView imgSearch, imgLocation, imgControls;
 
     @Override
     protected int getLayout() {
@@ -38,52 +20,17 @@ public class BrowseTaskFragment extends BaseFragment implements NetworkUtils.Net
 
     @Override
     protected void initView() {
-        lvList = (RecyclerView) findViewById(R.id.lvList);
-        lvManager = new LinearLayoutManager(getActivity());
-        workList = new ArrayList<>();
-
-
-        lvList.setLayoutManager(lvManager);
-
-        for (int i = 0; i < 10; i++) {
-            Work work = new Work();
-            work.setId(i);
-            work.setName("Hey ! Are you free tonight!");
-            work.setTime("2017-04-18T03:48:10+00:00");
-            work.setNew(true);
-            work.setDescription("15 phut truoc . Ha Noi . Phan loai : Cong nghe");
-            work.setPrice("500000");
-            workList.add(work);
-        }
-        workAdapter = new WorkAdapter(getActivity(), workList);
-        lvList.setAdapter(workAdapter);
-
-        lvList.addOnScrollListener(new EndlessRecyclerViewScrollListener(lvManager) {
-            @Override
-            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
-                Handler handler = new Handler();
-                handler.postDelayed(new TimerTask() {
-                    @Override
-                    public void run() {
-                        for (int i = 0; i < 10; i++) {
-                            Work work = new Work();
-                            work.setId(i);
-                            work.setName("Hey ! Are you free tonight!");
-                            work.setTime("2017-04-18T03:48:10+00:00");
-                            work.setNew(true);
-                            work.setDescription("15 phut truoc . Ha Noi . Phan loai : Cong nghe");
-                            work.setPrice("500000");
-                            workList.add(work);
-                        }
-                        workAdapter.notifyDataSetChanged();
-                    }
-                }, 2000);
-            }
-        });
+        imgSearch = (ImageView) findViewById(R.id.img_search);
+        imgLocation = (ImageView) findViewById(R.id.img_location);
+        imgControls = (ImageView) findViewById(R.id.img_controls);
     }
 
     @Override
     protected void initData() {
+        openFragment(R.id.find_task_container, ListTaskFragment.class, false);
+        imgControls.setOnClickListener(this);
+        imgLocation.setOnClickListener(this);
+        imgSearch.setOnClickListener(this);
 
     }
 
@@ -105,14 +52,7 @@ public class BrowseTaskFragment extends BaseFragment implements NetworkUtils.Net
     }
 
     @Override
-    public void onSuccess(JSONObject jsonResponse) {
-        if (jsonResponse != null) {
-            // String json = jsonResponse.toString();
-        }
-    }
-
-    @Override
-    public void onError() {
+    public void onClick(View view) {
 
     }
 }
---
+++ app/src/main/java/vn/tonish/hozo/fragment/ListTaskFragment.java
@@ -0,0 +1,89 @@
+package vn.tonish.hozo.fragment;
+
+
+import android.os.Handler;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TimerTask;
+
+import vn.tonish.hozo.R;
+import vn.tonish.hozo.adapter.WorkAdapter;
+import vn.tonish.hozo.model.Work;
+import vn.tonish.hozo.utils.EndlessRecyclerViewScrollListener;
+
+import static vn.tonish.hozo.R.id.lv_list;
+
+/**
+ * Created by Can Tran on 04/05/2017.
+ */
+
+public class ListTaskFragment extends BaseFragment {
+    private RecyclerView lvList;
+    private WorkAdapter workAdapter;
+    private LinearLayoutManager lvManager;
+    private List<Work> workList;
+
+    @Override
+    protected int getLayout() {
+        return R.layout.fragment_list_task;
+    }
+
+    @Override
+    protected void initView() {
+        lvList = (RecyclerView) findViewById(lv_list);
+    }
+
+    @Override
+    protected void initData() {
+        lvManager = new LinearLayoutManager(getActivity());
+        workList = new ArrayList<>();
+
+
+        lvList.setLayoutManager(lvManager);
+
+        for (int i = 0; i < 10; i++) {
+            Work work = new Work();
+            work.setId(i);
+            work.setName("Hey ! Are you free tonight!");
+            work.setTime("2017-04-18T03:48:10+00:00");
+            work.setNew(true);
+            work.setDescription("15 phut truoc . Ha Noi . Phan loai : Cong nghe");
+            work.setPrice("500000");
+            workList.add(work);
+        }
+        workAdapter = new WorkAdapter(getActivity(), workList);
+        lvList.setAdapter(workAdapter);
+
+        lvList.addOnScrollListener(new EndlessRecyclerViewScrollListener(lvManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                Handler handler = new Handler();
+                handler.postDelayed(new TimerTask() {
+                    @Override
+                    public void run() {
+                        for (int i = 0; i < 10; i++) {
+                            Work work = new Work();
+                            work.setId(i);
+                            work.setName("Hey ! Are you free tonight!");
+                            work.setTime("2017-04-18T03:48:10+00:00");
+                            work.setNew(true);
+                            work.setDescription("15 phut truoc . Ha Noi . Phan loai : Cong nghe");
+                            work.setPrice("500000");
+                            workList.add(work);
+                        }
+                        workAdapter.notifyDataSetChanged();
+                    }
+                }, 2000);
+            }
+        });
+
+    }
+
+    @Override
+    protected void resumeData() {
+
+    }
+}
