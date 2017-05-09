52c28d1ac51cfc815787da7dfe2828c43b119303
long bui <longbd@tonish.vn>
2017-05-09 14:12:05 +0700

    use retrofit to request server

--- app/src/main/java/vn/tonish/hozo/activity/other/ProfileActivity.java
+++ app/src/main/java/vn/tonish/hozo/activity/other/ProfileActivity.java
@@ -1,7 +1,6 @@
 package vn.tonish.hozo.activity.other;
 
 import android.content.Intent;
-import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -23,7 +22,6 @@ import vn.tonish.hozo.database.manager.UserManager;
 import vn.tonish.hozo.fragment.PosterReviewFragment;
 import vn.tonish.hozo.fragment.SelectTaskFragment;
 import vn.tonish.hozo.fragment.workerReviewFragment;
-import vn.tonish.hozo.network.NetworkConfig;
 import vn.tonish.hozo.network.NetworkUtils;
 import vn.tonish.hozo.utils.LogUtils;
 
