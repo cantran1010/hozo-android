8c9f30e88c1a80bbf6090fa170e5b6c3116a67b4
tonish1 <tonish>
2017-05-11 18:04:28 +0700

    edit reviews and Apicall, save review

--- app/src/main/java/vn/tonish/hozo/common/Constants.java
+++ app/src/main/java/vn/tonish/hozo/common/Constants.java
@@ -67,4 +67,6 @@ public class Constants {
     public static int PLACE_AUTOCOMPLETE_REQUEST_CODE = 876;
     public static final String REVIEW_TYPE_TASKER = "tasker";
     public static final String REVIEW_TYPE_POSTER = "poster";
+    public static final String REVIEW_TYPE_TASKER = "tasker";
+    public static final String REVIEW_TYPE_POSTER = "poster";
 }
