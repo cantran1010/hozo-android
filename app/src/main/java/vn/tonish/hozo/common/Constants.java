c125fba08da94ead78a33e527e725c42459487c2
tonish1 <tonish>
2017-05-12 08:30:13 +0700

    edit conflic rebase

--- app/src/main/java/vn/tonish/hozo/common/Constants.java
+++ app/src/main/java/vn/tonish/hozo/common/Constants.java
@@ -65,8 +65,7 @@ public class Constants {
 
     public static final int PERMISSION_REQUEST_CODE = 987;
     public static int PLACE_AUTOCOMPLETE_REQUEST_CODE = 876;
-    public static final String REVIEW_TYPE_TASKER = "tasker";
-    public static final String REVIEW_TYPE_POSTER = "poster";
+    
     public static final String REVIEW_TYPE_TASKER = "tasker";
     public static final String REVIEW_TYPE_POSTER = "poster";
 }
