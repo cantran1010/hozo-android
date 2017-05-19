5aa6e7f8008430684aa786ccc6d42a76ab36bcfc
Bui Long <longbd@tonish.vn>
2017-05-19 18:22:19 +0700

    my task

--- app/src/main/java/vn/tonish/hozo/fragment/MyTaskFragment.java
+++ app/src/main/java/vn/tonish/hozo/fragment/MyTaskFragment.java
@@ -1,11 +1,30 @@
 package vn.tonish.hozo.fragment;
 
 import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-
 import vn.tonish.hozo.R;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import vn.tonish.hozo.adapter.MyTaskAdapter;
+import vn.tonish.hozo.common.Constants;
+import vn.tonish.hozo.database.manager.UserManager;
+import vn.tonish.hozo.dialog.AlertDialogOkAndCancel;
+import vn.tonish.hozo.network.NetworkUtils;
+import vn.tonish.hozo.rest.ApiClient;
+import vn.tonish.hozo.rest.responseRes.TaskResponse;
+import vn.tonish.hozo.utils.DialogUtils;
+import vn.tonish.hozo.utils.LogUtils;
 import vn.tonish.hozo.view.TextViewHozo;
 
+import static android.content.ContentValues.TAG;
 import static vn.tonish.hozo.utils.Utils.setViewBackground;
 
 /**
@@ -13,7 +32,10 @@ import static vn.tonish.hozo.utils.Utils.setViewBackground;
  */
 
 public class MyTaskFragment extends BaseFragment implements View.OnClickListener {
-    private TextViewHozo layoutWorker, layoutPoster;
+    private TextViewHozo tvWorker, tvPoster;
+    private RecyclerView rcvTask;
+    private List<TaskResponse> taskResponses = new ArrayList<>();
+    private MyTaskAdapter myTaskAdapter;
 
     @Override
     protected int getLayout() {
@@ -22,16 +44,18 @@ public class MyTaskFragment extends BaseFragment implements View.OnClickListener
 
     @Override
     protected void initView() {
-        layoutWorker = (TextViewHozo) findViewById(R.id.layout_worker);
-        layoutPoster = (TextViewHozo) findViewById(R.id.layout_poster);
-        selectedTab(1);
+        tvWorker = (TextViewHozo) findViewById(R.id.tv_worker);
+        tvPoster = (TextViewHozo) findViewById(R.id.tv_poster);
 
+        rcvTask = (RecyclerView) findViewById(R.id.rcv_task);
     }
 
     @Override
     protected void initData() {
-        layoutWorker.setOnClickListener(this);
-        layoutPoster.setOnClickListener(this);
+        tvWorker.setOnClickListener(this);
+        tvPoster.setOnClickListener(this);
+
+        getTask("worker");
     }
 
     @Override
@@ -39,17 +63,87 @@ public class MyTaskFragment extends BaseFragment implements View.OnClickListener
 
     }
 
+    private void getTask(final String role) {
+        Map<String, String> params = new HashMap<>();
+        params.put("role", "poster");
+        LogUtils.d(TAG, "getTask start , param : " + params);
+
+        ApiClient.getApiService().getMyTask(UserManager.getUserToken(), params).enqueue(new Callback<List<TaskResponse>>() {
+            @Override
+            public void onResponse(Call<List<TaskResponse>> call, Response<List<TaskResponse>> response) {
+
+                LogUtils.d(TAG, "getTask code : " + response.code());
+                LogUtils.d(TAG, "getTask body : " + response.body());
+
+                if (response.code() == Constants.HTTP_CODE_OK) {
+                    taskResponses = response.body();
+                    refreshList();
+                } else if (response.code() == Constants.HTTP_CODE_UNAUTHORIZED) {
+                    NetworkUtils.RefreshToken(getActivity(), new NetworkUtils.RefreshListener() {
+                        @Override
+                        public void onRefreshFinish() {
+                            getTask(role);
+                        }
+                    });
+                } else {
+                    DialogUtils.showRetryDialog(getActivity(), new AlertDialogOkAndCancel.AlertDialogListener() {
+                        @Override
+                        public void onSubmit() {
+                            getTask(role);
+                        }
+
+                        @Override
+                        public void onCancel() {
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<TaskResponse>> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private void refreshList() {
+        if (myTaskAdapter == null) {
+            myTaskAdapter = new MyTaskAdapter(getActivity(), taskResponses);
+            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+            rcvTask.setLayoutManager(linearLayoutManager);
+            rcvTask.setAdapter(myTaskAdapter);
+
+//            lvList.addOnScrollListener(new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+//                @Override
+//                public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+//
+//                    LogUtils.d(TAG, "refreshList addOnScrollListener, page : " + page + " , totalItemsCount : " + totalItemsCount);
+//
+//                    if (isLoadingMoreFromDb) getCacheDataPage();
+//                    if (isLoadingMoreFromServer) getNotifications(true);
+//
+//                }
+//            });
+
+        } else {
+            myTaskAdapter.notifyDataSetChanged();
+        }
+
+        LogUtils.d(TAG, "refreshList , taskReponse size : " + taskResponses.size());
+    }
+
     private void selectedTab(int position) {
         if (position == 1) {
-            layoutWorker.setTextColor(ContextCompat.getColor(getActivity(), R.color.green));
-            setViewBackground(layoutWorker,ContextCompat.getDrawable(getContext(),R.drawable.bg_border_green));
-            layoutPoster.setTextColor(ContextCompat.getColor(getActivity(),R.color.gray));
-            setViewBackground(layoutPoster,ContextCompat.getDrawable(getContext(),R.drawable.bg_border_gray));
-        }else {
-            layoutWorker.setTextColor(ContextCompat.getColor(getActivity(), R.color.gray));
-            setViewBackground(layoutWorker,ContextCompat.getDrawable(getContext(),R.drawable.bg_border_gray));
-            layoutPoster.setTextColor(ContextCompat.getColor(getActivity(),R.color.green));
-            setViewBackground(layoutPoster,ContextCompat.getDrawable(getContext(),R.drawable.bg_border_green));
+            tvWorker.setTextColor(ContextCompat.getColor(getActivity(), R.color.white));
+            setViewBackground(tvWorker, ContextCompat.getDrawable(getContext(), R.drawable.my_task_worker_active));
+            tvPoster.setTextColor(ContextCompat.getColor(getActivity(), R.color.hozo_bg));
+            setViewBackground(tvPoster, ContextCompat.getDrawable(getContext(), R.drawable.my_task_poster_default));
+        } else {
+            tvWorker.setTextColor(ContextCompat.getColor(getActivity(), R.color.hozo_bg));
+            setViewBackground(tvWorker, ContextCompat.getDrawable(getContext(), R.drawable.my_task_worker_default));
+            tvPoster.setTextColor(ContextCompat.getColor(getActivity(), R.color.white));
+            setViewBackground(tvPoster, ContextCompat.getDrawable(getContext(), R.drawable.my_task_poster_active));
         }
     }
 
@@ -57,11 +151,11 @@ public class MyTaskFragment extends BaseFragment implements View.OnClickListener
     public void onClick(View v) {
 
         switch (v.getId()) {
-            case R.id.layout_worker:
+            case R.id.tv_worker:
                 selectedTab(1);
                 break;
 
-            case R.id.layout_poster:
+            case R.id.tv_poster:
                 selectedTab(2);
                 break;
 
