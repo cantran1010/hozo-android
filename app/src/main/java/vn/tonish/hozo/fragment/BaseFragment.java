7b97dde3b9b7f90b2d622b81d85a0a0c7ac8d354
Bui Long <longbd@tonish.vn>
2017-06-02 12:02:41 +0700

    restore my task screen

--- app/src/main/java/vn/tonish/hozo/fragment/BaseFragment.java
+++ app/src/main/java/vn/tonish/hozo/fragment/BaseFragment.java
@@ -5,16 +5,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.Serializable;
+import java.util.Stack;
+
 import vn.tonish.hozo.R;
 import vn.tonish.hozo.activity.BaseActivity;
 import vn.tonish.hozo.activity.MainActivity;
 import vn.tonish.hozo.common.Constants;
+import vn.tonish.hozo.utils.LogUtils;
 import vn.tonish.hozo.utils.TransitionScreen;
+
+import static android.content.ContentValues.TAG;
 
 /**
  * Created by LongBui on 4/12/17.
@@ -31,6 +39,7 @@ public abstract class BaseFragment extends Fragment implements SwipeRefreshLayou
 
     private View view;
     private SwipeRefreshLayout swipeRefreshLayout;
+    private Stack<StackEntry> fragmentsStack = new Stack<>();
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -133,6 +142,86 @@ public abstract class BaseFragment extends Fragment implements SwipeRefreshLayou
 
     void openFragment(int resId, Class<? extends Fragment> fragmentClazz, boolean addBackStack, TransitionScreen transitionScreen) {
         openFragment(resId, fragmentClazz, null, addBackStack, transitionScreen);
+    }
+
+    void showFragment(int resLayout, Class<?> newFragClass,
+                             boolean putStack, Bundle bundle,TransitionScreen transitionScreen) {
+        Activity activity = getActivity();
+        if (activity instanceof BaseActivity) {
+            BaseActivity baseActivity = (BaseActivity) activity;
+            baseActivity.showFragment(resLayout, newFragClass, putStack, bundle, transitionScreen);
+        }
+    }
+
+    public void showChildFragment(int resLayout, Class<?> newFragClass,
+                             boolean putStack, Bundle bundle, TransitionScreen transitionScreen) {
+
+        FragmentManager manager = getChildFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        TransitionScreen.setCustomAnimationsFragment(transaction, transitionScreen);
+
+        try {
+            Fragment newFragment = null;
+            String newTag = null;
+            if (newFragClass != null)
+                newTag = newFragClass.getName();
+
+            Fragment lastFragment = getLastFragment();
+            if (lastFragment != null) {
+                transaction.hide(lastFragment);
+            }
+
+            LogUtils.d(TAG, "showFragment , resLayout : " + resLayout + " , newFragClass.getName() : " + newFragClass.getName());
+
+            // find the fragment in fragment manager
+            newFragment = manager.findFragmentByTag(newTag);
+            if (newFragment != null && !newFragment.isRemoving()) {
+                transaction.show(newFragment).commit();
+            } else {
+                try {
+                    Fragment nFrag = (Fragment) newFragClass.newInstance();
+                    nFrag.setArguments(bundle);
+
+                    if (putStack) {
+                        transaction.add(resLayout, nFrag, newTag)
+                                .addToBackStack(newTag).commit();
+                    } else {
+                        transaction.add(resLayout, nFrag, newTag).commit();
+                    }
+
+                } catch (InstantiationException e) {
+                    e.printStackTrace();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            fragmentsStack.push(new StackEntry(newTag));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Fragment getLastFragment() {
+        if (fragmentsStack.isEmpty())
+            return null;
+        String fragTag = fragmentsStack.peek().getFragTag();
+        Fragment fragment = getChildFragmentManager().findFragmentByTag(
+                fragTag);
+        return fragment;
+    }
+
+    private static class StackEntry implements Serializable {
+        private String fragTag = null;
+
+        public StackEntry(String fragTag) {
+            super();
+            this.fragTag = fragTag;
+        }
+
+        public String getFragTag() {
+            return fragTag;
+        }
     }
 
     void updateMenuUi(int position) {
--- app/src/main/java/vn/tonish/hozo/fragment/MyTaskFragment.java
+++ app/src/main/java/vn/tonish/hozo/fragment/MyTaskFragment.java
@@ -1,37 +1,12 @@
 package vn.tonish.hozo.fragment;
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import vn.tonish.hozo.R;
-import vn.tonish.hozo.activity.TaskDetailActivity;
-import vn.tonish.hozo.adapter.MyTaskAdapter;
 import vn.tonish.hozo.common.Constants;
-import vn.tonish.hozo.database.entity.TaskEntity;
-import vn.tonish.hozo.database.manager.TaskManager;
-import vn.tonish.hozo.database.manager.UserManager;
-import vn.tonish.hozo.dialog.AlertDialogOkAndCancel;
-import vn.tonish.hozo.network.DataParse;
-import vn.tonish.hozo.network.NetworkUtils;
-import vn.tonish.hozo.rest.ApiClient;
-import vn.tonish.hozo.rest.responseRes.TaskResponse;
-import vn.tonish.hozo.utils.DialogUtils;
-import vn.tonish.hozo.utils.EndlessRecyclerViewScrollListener;
-import vn.tonish.hozo.utils.LogUtils;
 import vn.tonish.hozo.utils.TransitionScreen;
-import vn.tonish.hozo.utils.Utils;
 import vn.tonish.hozo.view.TextViewHozo;
 
 import static vn.tonish.hozo.utils.Utils.setViewBackground;
@@ -43,18 +18,8 @@ import static vn.tonish.hozo.utils.Utils.setViewBackground;
 public class MyTaskFragment extends BaseFragment implements View.OnClickListener {
     private static final String TAG = MyTaskFragment.class.getSimpleName();
     private TextViewHozo tvWorker, tvPoster;
-    private RecyclerView rcvTask;
-    private List<TaskResponse> taskResponses = new ArrayList<>();
-    private MyTaskAdapter myTaskAdapter;
-    private String role = Constants.ROLE_TASKER;
     public static final int LIMIT = 15;
-    private String sinceStr;
-    private Date sinceDate;
-    private String query;
-    boolean isLoadingMoreFromServer = true;
-    boolean isLoadingMoreFromDb = true;
-    boolean isLoadingFromServer = false;
-    private Call<List<TaskResponse>> call;
+    private String role = Constants.ROLE_TASKER;
 
     @Override
     protected int getLayout() {
@@ -65,237 +30,20 @@ public class MyTaskFragment extends BaseFragment implements View.OnClickListener
     protected void initView() {
         tvWorker = (TextViewHozo) findViewById(R.id.tv_worker);
         tvPoster = (TextViewHozo) findViewById(R.id.tv_poster);
-
-        rcvTask = (RecyclerView) findViewById(R.id.rcv_task);
-        createSwipeToRefresh();
     }
 
     @Override
     protected void initData() {
+
+        showChildFragment(R.id.layout_container_my_task, MyTaskWorkerFragment.class, false, new Bundle(), TransitionScreen.FADE_IN);
+
         tvWorker.setOnClickListener(this);
         tvPoster.setOnClickListener(this);
-
-        getCacheData(role, sinceDate);
-        getTaskFromServer(role, sinceStr, LIMIT, null);
-
-    }
-
-    private void getCacheData(String role, Date sinceDateIn) {
-        LogUtils.d(TAG, "getCacheData start , role : " + role + " , sinceDate : " + sinceDateIn);
-        List<TaskEntity> taskEntities = TaskManager.getTaskSince(sinceDateIn, role);
-        if (taskEntities.size() > 0)
-            sinceDate = taskEntities.get(taskEntities.size() - 1).getCreatedAt();
-        taskResponses.addAll(DataParse.converListTaskEntityToTaskResponse(taskEntities));
-        refreshList();
-        if (taskResponses.size() < LIMIT) isLoadingMoreFromDb = false;
-
-        LogUtils.d(TAG, "getCacheData , taskResponses : " + taskResponses.toString());
-        LogUtils.d(TAG, "getCacheData , taskResponses size : " + taskResponses.size());
     }
 
     @Override
     protected void resumeData() {
 
-    }
-
-    private void getTaskFromServer(final String role, final String since, final int limit, final String query) {
-
-        if (isLoadingFromServer) return;
-        isLoadingFromServer = true;
-        myTaskAdapter.stopLoadMore();
-
-        Map<String, String> params = new HashMap<>();
-
-        params.put("role", role);
-        if (since != null) params.put("since", since);
-        params.put("limit", limit + "");
-        if (query != null) params.put("query", query);
-
-        LogUtils.d(TAG, "getTaskFromServer start , param : " + params);
-
-        call = ApiClient.getApiService().getMyTask(UserManager.getUserToken(), params);
-        call.enqueue(new Callback<List<TaskResponse>>() {
-            @Override
-            public void onResponse(Call<List<TaskResponse>> call, Response<List<TaskResponse>> response) {
-
-                LogUtils.d(TAG, "getTaskFromServer code : " + response.code());
-                LogUtils.d(TAG, "getTaskFromServer body : " + response.body());
-
-                if (response.code() == Constants.HTTP_CODE_OK) {
-                    List<TaskResponse> taskResponsesBody = response.body();
-
-                    LogUtils.d(TAG, "getTaskFromServer taskResponsesBody size : " + taskResponsesBody.size());
-
-                    if (taskResponsesBody.size() > 0)
-                        sinceStr = taskResponsesBody.get(taskResponsesBody.size() - 1).getCreatedAt();
-
-                    for (int i = 0; i < taskResponsesBody.size(); i++)
-                        taskResponsesBody.get(i).setRole(role);
-
-                    for (int i = taskResponsesBody.size() - 1; i >= 0; i--)
-                        Utils.checkContainsTaskResponse(taskResponses, taskResponsesBody.get(i));
-
-                    TaskManager.insertTasks(DataParse.convertListTaskResponseToTaskEntity(taskResponsesBody));
-
-                    if (taskResponsesBody.size() < LIMIT) {
-                        myTaskAdapter.stopLoadMore();
-                        isLoadingMoreFromServer = false;
-                    }
-
-                    refreshList();
-
-                } else if (response.code() == Constants.HTTP_CODE_UNAUTHORIZED) {
-                    NetworkUtils.refreshToken(getActivity(), new NetworkUtils.RefreshListener() {
-                        @Override
-                        public void onRefreshFinish() {
-                            getTaskFromServer(role, since, limit, query);
-                        }
-                    });
-                } else {
-                    DialogUtils.showRetryDialog(getActivity(), new AlertDialogOkAndCancel.AlertDialogListener() {
-                        @Override
-                        public void onSubmit() {
-                            getTaskFromServer(role, since, limit, query);
-                        }
-
-                        @Override
-                        public void onCancel() {
-
-                        }
-                    });
-                }
-
-                isLoadingFromServer = false;
-                onStopRefresh();
-            }
-
-            @Override
-            public void onFailure(Call<List<TaskResponse>> call, Throwable t) {
-                LogUtils.e(TAG, "getTaskFromServer error : " + t.getMessage());
-//                DialogUtils.showRetryDialog(getActivity(), new AlertDialogOkAndCancel.AlertDialogListener() {
-//                    @Override
-//                    public void onSubmit() {
-//                        getTaskFromServer(role, since, limit, query);
-//                    }
-//
-//                    @Override
-//                    public void onCancel() {
-//
-//                    }
-//                });
-
-                isLoadingFromServer = false;
-                myTaskAdapter.stopLoadMore();
-                if (myTaskAdapter == null) myTaskAdapter.notifyDataSetChanged();
-                onStopRefresh();
-            }
-        });
-    }
-
-    private void refreshList() {
-        if (myTaskAdapter == null) {
-            myTaskAdapter = new MyTaskAdapter(getActivity(), taskResponses);
-            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
-            rcvTask.setLayoutManager(linearLayoutManager);
-            rcvTask.setAdapter(myTaskAdapter);
-
-            rcvTask.addOnScrollListener(new EndlessRecyclerViewScrollListener(linearLayoutManager) {
-                @Override
-                public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
-
-                    LogUtils.d(TAG, "refreshList addOnScrollListener, page : " + page + " , totalItemsCount : " + totalItemsCount);
-
-                    if (isLoadingMoreFromDb) getCacheData(role, sinceDate);
-                    if (isLoadingMoreFromServer) getTaskFromServer(role, sinceStr, LIMIT, query);
-
-                }
-            });
-
-            myTaskAdapter.setMyTaskAdapterListener(new MyTaskAdapter.MyTaskAdapterListener() {
-                @Override
-                public void onMyTaskAdapterClickListener(int position) {
-                    TaskResponse taskResponse = taskResponses.get(position);
-                    LogUtils.d(TAG, "myTaskAdapter.setMyTaskAdapterListener , taskResponse : " + taskResponse);
-
-                    Intent intent = new Intent(getActivity(), TaskDetailActivity.class);
-                    intent.putExtra(Constants.TASK_ID_EXTRA, taskResponse.getId());
-                    startActivityForResult(intent, Constants.REQUEST_CODE_TASK_EDIT, TransitionScreen.RIGHT_TO_LEFT);
-
-//                    if (role.equals(Constants.ROLE_TASKER)) {
-//                        if (taskResponse.getOfferStatus().equals(Constants.TASK_TYPE_BIDDER_PENDING)) {
-//                            Intent intent = new Intent(getActivity(), TaskDetailActivity.class);
-//                            intent.putExtra(Constants.TASK_ID_EXTRA, taskResponse.getId());
-//                            startActivity(intent, TransitionScreen.RIGHT_TO_LEFT);
-//                        } else if (taskResponse.getOfferStatus().equals(Constants.TASK_TYPE_BIDDER_ACCEPTED) && !taskResponse.getStatus().equals(Constants.TASK_TYPE_POSTER_COMPLETED)) {
-//                            Intent intent = new Intent(getActivity(), TaskDetailActivity.class);
-//                            intent.putExtra(Constants.TASK_ID_EXTRA, taskResponse.getId());
-//                            startActivity(intent, TransitionScreen.RIGHT_TO_LEFT);
-//                        } else if (taskResponse.getOfferStatus().equals(Constants.TASK_TYPE_BIDDER_ACCEPTED) && taskResponse.getStatus().equals(Constants.TASK_TYPE_POSTER_COMPLETED)) {
-//                            Intent intent = new Intent(getActivity(), TaskDetailActivity.class);
-//                            intent.putExtra(Constants.TASK_ID_EXTRA, taskResponse.getId());
-//                            startActivity(intent, TransitionScreen.RIGHT_TO_LEFT);
-//                        } else if (taskResponse.getOfferStatus().equals(Constants.TASK_TYPE_BIDDER_MISSED)) {
-//                            Intent intent = new Intent(getActivity(), TaskDetailActivity.class);
-//                            intent.putExtra(Constants.TASK_ID_EXTRA, taskResponse.getId());
-//                            startActivity(intent, TransitionScreen.RIGHT_TO_LEFT);
-//                        } else if (taskResponse.getOfferStatus().equals(Constants.TASK_TYPE_BIDDER_CANCELED)) {
-//                            Intent intent = new Intent(getActivity(), TaskDetailActivity.class);
-//                            intent.putExtra(Constants.TASK_ID_EXTRA, taskResponse.getId());
-//                            startActivity(intent, TransitionScreen.RIGHT_TO_LEFT);
-//                        }
-//                    } else if (role.equals(Constants.ROLE_POSTER)) {
-//                        if (taskResponse.getStatus().equals(Constants.TASK_TYPE_POSTER_OPEN)) {
-//                            Intent intent = new Intent(getActivity(), TaskDetailActivity.class);
-//                            intent.putExtra(Constants.TASK_ID_EXTRA, taskResponse.getId());
-//                            startActivityForResult(intent, Constants.REQUEST_CODE_TASK_EDIT, TransitionScreen.RIGHT_TO_LEFT);
-//                        } else if (taskResponse.getStatus().equals(Constants.TASK_TYPE_POSTER_ASSIGNED)) {
-//                            Intent intent = new Intent(getActivity(), TaskDetailActivity.class);
-//                            intent.putExtra(Constants.TASK_ID_EXTRA, taskResponse.getId());
-//                            startActivity(intent, TransitionScreen.RIGHT_TO_LEFT);
-//                        } else if (taskResponse.getStatus().equals(Constants.TASK_TYPE_POSTER_COMPLETED)) {
-//                            Intent intent = new Intent(getActivity(), TaskDetailActivity.class);
-//                            intent.putExtra(Constants.TASK_ID_EXTRA, taskResponse.getId());
-//                            startActivity(intent, TransitionScreen.RIGHT_TO_LEFT);
-//                        } else if (taskResponse.getStatus().equals(Constants.TASK_TYPE_POSTER_OVERDUE)) {
-//                            Intent intent = new Intent(getActivity(), TaskDetailActivity.class);
-//                            intent.putExtra(Constants.TASK_ID_EXTRA, taskResponse.getId());
-//                            startActivity(intent, TransitionScreen.RIGHT_TO_LEFT);
-//                        } else if (taskResponse.getStatus().equals(Constants.TASK_TYPE_POSTER_CANCELED)) {
-//                            Intent intent = new Intent(getActivity(), TaskDetailActivity.class);
-//                            intent.putExtra(Constants.TASK_ID_EXTRA, taskResponse.getId());
-//                            startActivity(intent, TransitionScreen.RIGHT_TO_LEFT);
-//                        }
-//                    }
-
-                }
-            });
-
-        } else {
-
-            myTaskAdapter.notifyDataSetChanged();
-//            rcvTask.post(new Runnable() {
-//                public void run() {
-//                    myTaskAdapter.notifyItemInserted(taskResponses.size() - 2);
-//                }
-//            });
-        }
-
-        LogUtils.d(TAG, "refreshList , taskReponse size : " + taskResponses.size());
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == Constants.REQUEST_CODE_TASK_EDIT && resultCode == Constants.RESULT_CODE_TASK_EDIT) {
-            TaskResponse taskEdit = (TaskResponse) data.getSerializableExtra(Constants.EXTRA_TASK);
-            for (int i = 0; i < taskResponses.size(); i++) {
-                if (taskResponses.get(i).getId() == taskEdit.getId()) {
-                    taskResponses.set(i, taskEdit);
-                    myTaskAdapter.notifyDataSetChanged();
-                    break;
-                }
-            }
-        }
     }
 
     private void selectedTab(int position) {
@@ -318,52 +66,19 @@ public class MyTaskFragment extends BaseFragment implements View.OnClickListener
         switch (v.getId()) {
             case R.id.tv_worker:
                 if (role.equals(Constants.ROLE_TASKER)) break;
-                resetSelectedTab(1);
+                role = Constants.ROLE_TASKER;
+                showChildFragment(R.id.layout_container_my_task, MyTaskWorkerFragment.class, false, new Bundle(), TransitionScreen.RIGHT_TO_LEFT);
                 selectedTab(1);
                 break;
 
             case R.id.tv_poster:
                 if (role.equals(Constants.ROLE_POSTER)) break;
-                resetSelectedTab(2);
+                role = Constants.ROLE_POSTER;
+                showChildFragment(R.id.layout_container_my_task, MyTaskPosterFragment.class, false, new Bundle(), TransitionScreen.LEFT_TO_RIGHT);
                 selectedTab(2);
                 break;
 
         }
     }
-
-    private void resetSelectedTab(int position) {
-
-        switch (position) {
-            case 1:
-                role = Constants.ROLE_TASKER;
-                break;
-
-            case 2:
-                role = Constants.ROLE_POSTER;
-                break;
-
-            default:
-                role = Constants.ROLE_TASKER;
-                break;
-        }
-
-        call.cancel();
-        taskResponses.clear();
-        myTaskAdapter = null;
-        sinceStr = null;
-        sinceDate = null;
-        query = null;
-        isLoadingMoreFromServer = true;
-        isLoadingMoreFromDb = true;
-        isLoadingFromServer = false;
-
-        getCacheData(role, sinceDate);
-        getTaskFromServer(role, sinceStr, LIMIT, null);
-    }
-
-    @Override
-    public void onRefresh() {
-        super.onRefresh();
-        getTaskFromServer(role, null, LIMIT, null);
-    }
 }
+
