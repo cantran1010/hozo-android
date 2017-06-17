0b23a7f67dd18aefbb6eb7154b0fdb7884c7b632
long bui <longbd@tonish.vn>
2017-06-17 16:29:15 +0700

    fix crash,clear all notification when logout

--- app/src/main/java/vn/tonish/hozo/activity/HomeActivity.java
+++ app/src/main/java/vn/tonish/hozo/activity/HomeActivity.java
@@ -1,8 +1,10 @@
 package vn.tonish.hozo.activity;
 
 import vn.tonish.hozo.R;
+import vn.tonish.hozo.common.Constants;
 import vn.tonish.hozo.fragment.LoginFragment;
 import vn.tonish.hozo.utils.TransitionScreen;
+import vn.tonish.hozo.utils.Utils;
 
 /**
  * Created by CanTran on 5/9/17.
@@ -21,6 +23,11 @@ public class HomeActivity extends BaseActivity {
 
     @Override
     protected void initData() {
+
+        if (getIntent().hasExtra(Constants.LOGOUT_EXTRA))
+            if (getIntent().getBooleanExtra(Constants.LOGOUT_EXTRA, true))
+                Utils.cancelAllNotification(this);
+
         openFragment(R.id.layout_container, LoginFragment.class, false, TransitionScreen.FADE_IN);
     }
 
