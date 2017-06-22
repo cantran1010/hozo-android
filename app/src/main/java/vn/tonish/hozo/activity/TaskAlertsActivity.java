f7108b957f9323c2fc83a51d43e64257b0c785c2
tonish1 <tonish>
2017-06-22 17:44:48 +0700

    task alerts

--- app/src/main/java/vn/tonish/hozo/activity/TaskAlertsActivity.java
+++ app/src/main/java/vn/tonish/hozo/activity/TaskAlertsActivity.java
@@ -1,15 +1,26 @@
 package vn.tonish.hozo.activity;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+
 import vn.tonish.hozo.R;
+import vn.tonish.hozo.adapter.TaskAlertsAdapter;
+import vn.tonish.hozo.database.entity.CategoryEntity;
+import vn.tonish.hozo.database.manager.CategoryManager;
+import vn.tonish.hozo.model.TaskAlert;
 
 /**
  * Created by CanTran on 6/21/17.
  */
 
 public class TaskAlertsActivity extends BaseActivity implements View.OnClickListener {
+    private ArrayList<TaskAlert> taskAlerts;
+    private TaskAlertsAdapter taskAlertsAdapter;
+
     @Override
     protected int getLayout() {
         return R.layout.activity_task_alerts;
@@ -19,10 +30,23 @@ public class TaskAlertsActivity extends BaseActivity implements View.OnClickList
     protected void initView() {
         ImageView imgBack = (ImageView) findViewById(R.id.img_back);
         imgBack.setOnClickListener(this);
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.rcv_task_alert);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        taskAlerts = new ArrayList<>();
+        taskAlertsAdapter = new TaskAlertsAdapter(taskAlerts);
+        mRecyclerView.setAdapter(taskAlertsAdapter);
     }
 
     @Override
     protected void initData() {
+        if (!(CategoryManager.getAllCategories() == null || CategoryManager.getAllCategories().size() == 0))
+            for (CategoryEntity categoryEntity : CategoryManager.getAllCategories()
+                    ) {
+                TaskAlert taskAlert = new TaskAlert();
+                taskAlert.setName(categoryEntity.getName());
+                taskAlert.setSelected(true);
+                taskAlerts.add(taskAlert);
+            }
 
 
     }
---
+++ app/src/main/java/vn/tonish/hozo/model/TaskAlert.java
@@ -0,0 +1,26 @@
+package vn.tonish.hozo.model;
+
+/**
+ * Created by CanTran on 6/21/17.
+ */
+
+public class TaskAlert {
+    private String name;
+    private boolean  selected;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isSelected() {
+        return selected;
+    }
+
+    public void setSelected(boolean selected) {
+        this.selected = selected;
+    }
+}
