484f37113ede555ac0f155edc9913ace478b2752
tonish1 <tonish>
2017-07-07 17:24:37 +0700

    update_ui_profile

---
+++ app/src/main/java/vn/tonish/hozo/activity/GiveInforActivity.java
@@ -0,0 +1,27 @@
+package vn.tonish.hozo.activity;
+
+import vn.tonish.hozo.R;
+
+public class GiveInforActivity extends BaseActivity {
+
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_give_info;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void resumeData() {
+
+    }
+}
