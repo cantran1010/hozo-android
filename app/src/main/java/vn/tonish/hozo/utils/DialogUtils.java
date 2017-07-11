9cd607f42541f6f9975b07fb73710b703a5661a7
Bui Long <longbd@tonish.vn>
2017-07-11 09:55:54 +0700

    fix crash on fabric

--- app/src/main/java/vn/tonish/hozo/utils/DialogUtils.java
+++ app/src/main/java/vn/tonish/hozo/utils/DialogUtils.java
@@ -13,26 +13,32 @@ import vn.tonish.hozo.dialog.AlertDialogOkFullScreen;
 public class DialogUtils {
 
     public static void showRetryDialog(Context context, AlertDialogOkAndCancel.AlertDialogListener alertDialogListener) {
+        if (context == null) return;
         new AlertDialogOkAndCancel(context, context.getString(R.string.retry_title), context.getString(R.string.retry_content), context.getString(R.string.retry), context.getString(R.string.report_cancel), alertDialogListener);
     }
 
     public static void showOkAndCancelDialog(Context context, String title, String content, String submit, String cancel, AlertDialogOkAndCancel.AlertDialogListener alertDialogListener) {
+        if (context == null) return;
         new AlertDialogOkAndCancel(context, title, content, submit, cancel, alertDialogListener);
     }
 
     public static void showOkDialog(Context context, String title, String content, String submit, AlertDialogOk.AlertDialogListener alertDialogListener) {
+        if (context == null) return;
         new AlertDialogOk(context, title, content, submit, alertDialogListener);
     }
 
     public static void showOkDialogFullScreen(Context context, String title, String content, String submit, AlertDialogOkFullScreen.AlertDialogListener alertDialogListener) {
+        if (context == null) return;
         new AlertDialogOkFullScreen(context, title, content, submit, alertDialogListener);
     }
 
     public static void showReCommendUpdateDialog(Context context, AlertDialogOkAndCancel.AlertDialogListener alertDialogListener) {
+        if (context == null) return;
         new AlertDialogOkAndCancel(context, context.getString(R.string.update_version), context.getString(R.string.update_content), context.getString(R.string.oke), context.getString(R.string.report_cancel), alertDialogListener);
     }
 
     public static void showForceUpdateDialog(Context context, AlertDialogOkFullScreen.AlertDialogListener alertDialogListener) {
+        if (context == null) return;
         new AlertDialogOkFullScreen(context, context.getString(R.string.update_version), context.getString(R.string.update_force_content), context.getString(R.string.update_oke), alertDialogListener);
     }
 
