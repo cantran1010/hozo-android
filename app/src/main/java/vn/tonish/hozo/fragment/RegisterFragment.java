75cbed5d54832e0bf2fdcfeb8ee76447ae0e4c08
Bui Long <longbd@tonish.vn>
2017-08-07 10:15:44 +0700

    update register screen

--- app/src/main/java/vn/tonish/hozo/fragment/RegisterFragment.java
+++ app/src/main/java/vn/tonish/hozo/fragment/RegisterFragment.java
@@ -1,8 +1,14 @@
 package vn.tonish.hozo.fragment;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.DatePicker;
@@ -12,33 +18,46 @@ import android.widget.RelativeLayout;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.util.Calendar;
 import java.util.Date;
 
 import io.realm.Realm;
 import okhttp3.MediaType;
+import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import vn.tonish.hozo.R;
+import vn.tonish.hozo.activity.AlbumActivity;
+import vn.tonish.hozo.activity.CropImageActivity;
 import vn.tonish.hozo.activity.MainActivity;
+import vn.tonish.hozo.activity.PreviewImageActivity;
 import vn.tonish.hozo.common.Constants;
 import vn.tonish.hozo.database.entity.UserEntity;
 import vn.tonish.hozo.database.manager.UserManager;
 import vn.tonish.hozo.dialog.AlertDialogOkAndCancel;
+import vn.tonish.hozo.dialog.PickImageDialog;
 import vn.tonish.hozo.network.NetworkUtils;
 import vn.tonish.hozo.rest.ApiClient;
 import vn.tonish.hozo.rest.responseRes.APIError;
 import vn.tonish.hozo.rest.responseRes.ErrorUtils;
+import vn.tonish.hozo.rest.responseRes.ImageResponse;
 import vn.tonish.hozo.utils.DateTimeUtils;
+import vn.tonish.hozo.utils.DialogUtils;
+import vn.tonish.hozo.utils.FileUtils;
 import vn.tonish.hozo.utils.LogUtils;
 import vn.tonish.hozo.utils.ProgressDialogUtils;
 import vn.tonish.hozo.utils.TransitionScreen;
 import vn.tonish.hozo.utils.Utils;
+import vn.tonish.hozo.view.CircleImageView;
 import vn.tonish.hozo.view.EdittextHozo;
 import vn.tonish.hozo.view.TextViewHozo;
 
+import static vn.tonish.hozo.R.id.img_avatar;
+import static vn.tonish.hozo.common.Constants.REQUEST_CODE_PICK_IMAGE;
+import static vn.tonish.hozo.common.Constants.RESPONSE_CODE_PICK_IMAGE;
 import static vn.tonish.hozo.utils.DateTimeUtils.getOnlyIsoFromDate;
 import static vn.tonish.hozo.utils.DialogUtils.showRetryDialog;
 
@@ -48,14 +67,19 @@ import static vn.tonish.hozo.utils.DialogUtils.showRetryDialog;
 
 public class RegisterFragment extends BaseFragment implements View.OnClickListener {
     private final static String TAG = RegisterFragment.class.getName();
-    private EdittextHozo edtName, edtAddress;
+    private EdittextHozo edtName, edtAddress, edtDes;
     private TextViewHozo btnSave;
     private TextViewHozo tvMale, tvFemale;
     private ImageView imgMale, imgFemale;
     private TextViewHozo tvBirthday;
     private String gender;
     private final Calendar calendar = Calendar.getInstance();
-
+    private CircleImageView imgAvatar;
+    private final String[] permissions = new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    private String imgPath;
+    private File file;
+    private int avataId;
+    private boolean isUpdateAvata = false;
 
     @Override
     protected int getLayout() {
@@ -70,6 +94,11 @@ public class RegisterFragment extends BaseFragment implements View.OnClickListen
         imgFemale = (ImageView) findViewById(R.id.img_female);
         tvBirthday = (TextViewHozo) findViewById(R.id.tv_birthday);
         edtAddress = (EdittextHozo) findViewById(R.id.edt_address);
+        edtDes = (EdittextHozo) findViewById(R.id.edt_description);
+
+        ImageView imgCamera = (ImageView) findViewById(R.id.img_camera);
+        imgCamera.setOnClickListener(this);
+
         RelativeLayout layoutMale = (RelativeLayout) findViewById(R.id.layout_male);
         layoutMale.setOnClickListener(this);
         RelativeLayout layoutBirthday = (RelativeLayout) findViewById(R.id.layout_birthday);
@@ -81,6 +110,8 @@ public class RegisterFragment extends BaseFragment implements View.OnClickListen
         btnSave = (TextViewHozo) findViewById(R.id.btn_save);
         btnSave.setOnClickListener(this);
 
+        imgAvatar = (CircleImageView) findViewById(img_avatar);
+        imgAvatar.setOnClickListener(this);
     }
 
     @Override
@@ -99,12 +130,7 @@ public class RegisterFragment extends BaseFragment implements View.OnClickListen
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_save:
-                String name = edtName.getText().toString().trim();
-                if (name.isEmpty()) {
-                    edtName.setError(getActivity().getString(R.string.erro_empty_name));
-                } else {
-                    updateInfor();
-                }
+                doSave();
                 break;
             case R.id.layout_birthday:
                 openDatePicker();
@@ -120,6 +146,198 @@ public class RegisterFragment extends BaseFragment implements View.OnClickListen
                 updateGender(getString(R.string.gender_female));
                 break;
 
+            case R.id.img_camera:
+                doPickImage();
+                break;
+
+            case img_avatar:
+                if (file != null) {
+                    Intent intentView = new Intent(getActivity(), PreviewImageActivity.class);
+                    intentView.putExtra(Constants.EXTRA_IMAGE_PATH, file.getPath());
+                    startActivity(intentView, TransitionScreen.RIGHT_TO_LEFT);
+                }
+                break;
+
+        }
+
+    }
+
+    private void doSave() {
+
+        String name = edtName.getText().toString().trim();
+        if (name.isEmpty()) {
+            edtName.requestFocus();
+            edtName.setError(getActivity().getString(R.string.erro_empty_name));
+            return;
+        } else if (edtDes.getText().toString().trim().length() > 500) {
+            edtDes.requestFocus();
+            edtDes.setError(getString(R.string.error_des));
+            return;
+        } else if (edtAddress.getText().toString().trim().equals("")) {
+            edtAddress.requestFocus();
+            edtAddress.setError(getActivity().getString(R.string.error_address));
+            return;
+        }
+
+        if (isUpdateAvata) {
+            updateAvata();
+        } else {
+            updateInfor();
+        }
+    }
+
+    private void updateAvata() {
+
+        ProgressDialogUtils.showProgressDialog(getActivity());
+
+        final RequestBody requestBody = RequestBody.create(MediaType.parse("image/*"), file);
+        MultipartBody.Part itemPart = MultipartBody.Part.createFormData("image", file.getName(), requestBody);
+
+        ApiClient.getApiService().uploadImage(UserManager.getUserToken(), itemPart).enqueue(new Callback<ImageResponse>() {
+            @Override
+            public void onResponse(Call<ImageResponse> call, Response<ImageResponse> response) {
+                LogUtils.d(TAG, "uploadImage onResponse : " + response.body());
+                LogUtils.d(TAG, "uploadImage code : " + response.code());
+
+                if (response.code() == Constants.HTTP_CODE_CREATED) {
+                    ImageResponse imageResponse = response.body();
+                    avataId = imageResponse != null ? imageResponse.getIdTemp() : 0;
+                    updateInfor();
+                } else if (response.code() == Constants.HTTP_CODE_UNAUTHORIZED) {
+                    NetworkUtils.refreshToken(getActivity(), new NetworkUtils.RefreshListener() {
+                        @Override
+                        public void onRefreshFinish() {
+                            updateAvata();
+                        }
+                    });
+                    ProgressDialogUtils.dismissProgressDialog();
+                } else if (response.code() == Constants.HTTP_CODE_BLOCK_USER) {
+                    ProgressDialogUtils.dismissProgressDialog();
+                    Utils.blockUser(getActivity());
+                } else {
+                    DialogUtils.showRetryDialog(getActivity(), new AlertDialogOkAndCancel.AlertDialogListener() {
+                        @Override
+                        public void onSubmit() {
+                            updateAvata();
+                        }
+
+                        @Override
+                        public void onCancel() {
+
+                        }
+                    });
+                    ProgressDialogUtils.dismissProgressDialog();
+
+                }
+                FileUtils.deleteDirectory(new File(FileUtils.OUTPUT_DIR));
+            }
+
+            @Override
+            public void onFailure(Call<ImageResponse> call, Throwable t) {
+                LogUtils.e(TAG, "uploadImage onFailure : " + t.getMessage());
+                DialogUtils.showRetryDialog(getActivity(), new AlertDialogOkAndCancel.AlertDialogListener() {
+                    @Override
+                    public void onSubmit() {
+                        updateAvata();
+                    }
+
+                    @Override
+                    public void onCancel() {
+
+                    }
+                });
+                FileUtils.deleteDirectory(new File(FileUtils.OUTPUT_DIR));
+                ProgressDialogUtils.dismissProgressDialog();
+            }
+        });
+    }
+
+    private void doPickImage() {
+        checkPermission();
+    }
+
+    private void checkPermission() {
+        if (ContextCompat.checkSelfPermission(getActivity(),
+                Manifest.permission.CAMERA) + ContextCompat
+                .checkSelfPermission(getActivity(),
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(permissions, Constants.PERMISSION_REQUEST_CODE);
+        } else {
+            permissionGranted();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        LogUtils.d(TAG, "onRequestPermissionsResult start");
+        if (requestCode != Constants.PERMISSION_REQUEST_CODE
+                || grantResults.length == 0
+                || grantResults[0] == PackageManager.PERMISSION_DENIED) {
+            permissionDenied();
+        } else {
+            permissionGranted();
+        }
+    }
+
+    private void permissionGranted() {
+
+        PickImageDialog pickImageDialog = new PickImageDialog(getActivity());
+        pickImageDialog.setPickImageListener(new PickImageDialog.PickImageListener() {
+            @Override
+            public void onCamera() {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, setImageUri());
+                startActivityForResult(cameraIntent, Constants.REQUEST_CODE_CAMERA);
+            }
+
+            @Override
+            public void onGallery() {
+                Intent intent = new Intent(getActivity(), AlbumActivity.class);
+                intent.putExtra(Constants.EXTRA_ONLY_IMAGE, true);
+                intent.putExtra(Constants.EXTRA_IS_CROP_PROFILE, true);
+                startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE, TransitionScreen.RIGHT_TO_LEFT);
+            }
+        });
+        pickImageDialog.showView();
+
+    }
+
+    private void permissionDenied() {
+        LogUtils.d(TAG, "permissionDenied camera");
+    }
+
+    private Uri setImageUri() {
+        File file = new File(FileUtils.getInstance().getHozoDirectory(), "image" + System.currentTimeMillis() + ".jpg");
+        Uri imgUri = Uri.fromFile(file);
+        this.imgPath = file.getAbsolutePath();
+        return imgUri;
+    }
+
+    private String getImagePath() {
+        return imgPath;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_PICK_IMAGE
+                && resultCode == RESPONSE_CODE_PICK_IMAGE
+                && data != null) {
+            String imgPath = data.getStringExtra(Constants.EXTRA_IMAGE_PATH);
+            Utils.displayImage(getActivity(), imgAvatar, imgPath);
+            file = new File(imgPath);
+            isUpdateAvata = true;
+        } else if (requestCode == Constants.REQUEST_CODE_CAMERA && resultCode == Activity.RESULT_OK) {
+            Intent intent = new Intent(getActivity(), CropImageActivity.class);
+            intent.putExtra(Constants.EXTRA_IMAGE_PATH, getImagePath());
+            startActivityForResult(intent, Constants.REQUEST_CODE_CROP_IMAGE, TransitionScreen.RIGHT_TO_LEFT);
+        } else if (requestCode == Constants.REQUEST_CODE_CROP_IMAGE && resultCode == Constants.RESPONSE_CODE_CROP_IMAGE) {
+            String imgPath = data != null ? data.getStringExtra(Constants.EXTRA_IMAGE_PATH) : null;
+            Utils.displayImage(getActivity(), imgAvatar, null);
+            file = new File(imgPath != null ? imgPath : null);
+            isUpdateAvata = true;
         }
 
     }
@@ -169,12 +387,16 @@ public class RegisterFragment extends BaseFragment implements View.OnClickListen
         try {
             jsonRequest.put(Constants.PARAMETER_FULL_NAME, edtName.getText().toString());
             jsonRequest.put(Constants.PARAMETER_ADDRESS, edtAddress.getText().toString());
+            jsonRequest.put(Constants.PARAMETER_DESCRIPTION, edtDes.getText().toString().trim());
 
             if (!tvBirthday.getText().toString().equals(""))
                 jsonRequest.put(Constants.PARAMETER_DATE_OF_BIRTH, getOnlyIsoFromDate(tvBirthday.getText().toString()));
 
             if (gender != null)
                 jsonRequest.put(Constants.PARAMETER_GENDER, gender);
+
+            if (isUpdateAvata)
+                jsonRequest.put(Constants.PARAMETER_AVATA_ID, avataId);
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -221,7 +443,6 @@ public class RegisterFragment extends BaseFragment implements View.OnClickListen
             @Override
             public void onFailure(Call<UserEntity> call, Throwable t) {
                 LogUtils.e(TAG, "onFailure message : " + t.getMessage());
-                ProgressDialogUtils.dismissProgressDialog();
                 showRetryDialog(getContext(), new AlertDialogOkAndCancel.AlertDialogListener() {
                     @Override
                     public void onSubmit() {
@@ -233,6 +454,7 @@ public class RegisterFragment extends BaseFragment implements View.OnClickListen
 
                     }
                 });
+                ProgressDialogUtils.dismissProgressDialog();
             }
         });
     }
