c3e788805c4df7e153cbd8896fb7ae9868ac3ad8
Bui Long <longbd@tonish.vn>
2017-11-03 15:49:14 +0700

    request api support

--- app/src/main/java/vn/tonish/hozo/fragment/support/SupportRateFragment.java
+++ app/src/main/java/vn/tonish/hozo/fragment/support/SupportRateFragment.java
@@ -5,8 +5,23 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.RatingBar;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import vn.tonish.hozo.R;
+import vn.tonish.hozo.common.Constants;
+import vn.tonish.hozo.database.manager.UserManager;
+import vn.tonish.hozo.dialog.AlertDialogOkAndCancel;
 import vn.tonish.hozo.fragment.BaseFragment;
+import vn.tonish.hozo.rest.ApiClient;
+import vn.tonish.hozo.utils.DialogUtils;
+import vn.tonish.hozo.utils.LogUtils;
+import vn.tonish.hozo.utils.ProgressDialogUtils;
 import vn.tonish.hozo.utils.Utils;
 import vn.tonish.hozo.view.ButtonHozo;
 import vn.tonish.hozo.view.EdittextHozo;
@@ -73,12 +88,75 @@ public class SupportRateFragment extends BaseFragment implements View.OnClickLis
 
     }
 
+    private void doRate() {
+
+        if (ratingBar.getRating() == 0) {
+            Utils.showLongToast(getActivity(), getString(R.string.rate_miss_star), true, false);
+            return;
+        }
+
+        ProgressDialogUtils.showProgressDialog(getActivity());
+        final JSONObject jsonRequest = new JSONObject();
+        try {
+            jsonRequest.put("body", edtContent.getText().toString().trim());
+            jsonRequest.put("rating", ratingBar.getRating());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        LogUtils.d(TAG, "doRate data request : " + jsonRequest.toString());
+        RequestBody body = RequestBody.create(MediaType.parse("application/json"), jsonRequest.toString());
+
+        ApiClient.getApiService().sendFeedback(UserManager.getUserToken(), body).enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> response) {
+
+                LogUtils.d(TAG, "doRate onResponse : " + response.body());
+                LogUtils.d(TAG, "doRate code : " + response.code());
+
+                if (response.code() == Constants.HTTP_CODE_OK) {
+                    Utils.showLongToast(getActivity(), getString(R.string.rate_success), false, false);
+                } else {
+                    DialogUtils.showRetryDialog(getActivity(), new AlertDialogOkAndCancel.AlertDialogListener() {
+                        @Override
+                        public void onSubmit() {
+                            doRate();
+                        }
+
+                        @Override
+                        public void onCancel() {
+
+                        }
+                    });
+                }
+                ProgressDialogUtils.dismissProgressDialog();
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable t) {
+                DialogUtils.showRetryDialog(getActivity(), new AlertDialogOkAndCancel.AlertDialogListener() {
+                    @Override
+                    public void onSubmit() {
+                        doRate();
+                    }
+
+                    @Override
+                    public void onCancel() {
+
+                    }
+                });
+                ProgressDialogUtils.dismissProgressDialog();
+            }
+        });
+
+    }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
 
             case R.id.btn_send:
-
+                doRate();
                 break;
 
             case R.id.tv_go_play_store:
