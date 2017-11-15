66440a449fa6bdbc37f7000a45a5d0760aeb97ad
tonish1 <tonish>
2017-11-15 19:19:40 +0700

    rebase and fix warning +done

--- app/src/main/java/vn/tonish/hozo/fragment/BrowseTaskFragment.java
+++ app/src/main/java/vn/tonish/hozo/fragment/BrowseTaskFragment.java
@@ -31,7 +31,6 @@ import vn.tonish.hozo.activity.BrowserTaskMapActivity;
 import vn.tonish.hozo.activity.MainActivity;
 import vn.tonish.hozo.activity.SettingActivity;
 import vn.tonish.hozo.activity.task_detail.DetailTaskActivity;
-import vn.tonish.hozo.activity.TaskDetailNewActivity;
 import vn.tonish.hozo.adapter.ScaleInAnimationAdapter;
 import vn.tonish.hozo.adapter.TaskAdapter;
 import vn.tonish.hozo.common.Constants;
