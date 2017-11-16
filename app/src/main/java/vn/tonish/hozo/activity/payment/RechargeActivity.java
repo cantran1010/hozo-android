82af48f6544fdf87c4b140cddee604b395113367
Bui Long <longbd@tonish.vn>
2017-11-16 11:43:57 +0700

    my wallet + history payment

--- app/src/main/java/vn/tonish/hozo/activity/payment/RechargeActivity.java
+++ app/src/main/java/vn/tonish/hozo/activity/payment/RechargeActivity.java
@@ -1,16 +1,12 @@
 package vn.tonish.hozo.activity.payment;
 
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.Spinner;
+import android.widget.LinearLayout;
 
 import vn.tonish.hozo.R;
 import vn.tonish.hozo.activity.BaseActivity;
 import vn.tonish.hozo.utils.TransitionScreen;
-import vn.tonish.hozo.view.ButtonHozo;
-import vn.tonish.hozo.view.EdittextHozo;
 
 /**
  * Created by LongBui on 10/10/17.
@@ -20,9 +16,7 @@ public class RechargeActivity extends BaseActivity implements View.OnClickListen
 
     private static final String TAG = RechargeActivity.class.getSimpleName();
     private ImageView imgBack;
-    private ButtonHozo btnRecharge;
-    private Spinner spType;
-    private EdittextHozo edtMoney;
+    private LinearLayout promotionLayout, atmLayout;
 
     @Override
     protected int getLayout() {
@@ -34,41 +28,19 @@ public class RechargeActivity extends BaseActivity implements View.OnClickListen
         imgBack = (ImageView) findViewById(R.id.img_back);
         imgBack.setOnClickListener(this);
 
-        btnRecharge = (ButtonHozo) findViewById(R.id.btn_recharge);
-        btnRecharge.setOnClickListener(this);
+        promotionLayout = (LinearLayout) findViewById(R.id.promotion_layout);
+        promotionLayout.setOnClickListener(this);
 
-        spType = (Spinner) findViewById(R.id.sp_type);
-
-        edtMoney = (EdittextHozo) findViewById(R.id.edt_money);
     }
 
     @Override
     protected void initData() {
-        edtMoney.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
     }
 
     @Override
     protected void resumeData() {
 
-    }
-
-    private String getLongEdittext(EdittextHozo edittextHozo) {
-        return edittextHozo.getText().toString().replace(",", "").replace(".", "");
     }
 
     @Override
@@ -79,21 +51,10 @@ public class RechargeActivity extends BaseActivity implements View.OnClickListen
                 finish();
                 break;
 
-            case R.id.btn_recharge:
-
-                switch (spType.getSelectedItemPosition()){
-
-                    case 0:
-                        startActivity(PromotionCodeActivity.class, TransitionScreen.RIGHT_TO_LEFT);
-                        break;
-
-                    case 1:
-
-                        break;
-
-                }
-
+            case R.id.promotion_layout:
+                startActivity(PromotionCodeActivity.class, TransitionScreen.RIGHT_TO_LEFT);
                 break;
+
 
         }
     }
