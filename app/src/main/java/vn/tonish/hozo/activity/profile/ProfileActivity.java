86dd7d317236f48ffcc66bb4f0b2b5f17fd403b1
Bui Long <longbd@tonish.vn>
2017-12-05 16:11:41 +0700

    fix findtask + payment layout

--- app/src/main/java/vn/tonish/hozo/activity/profile/ProfileActivity.java
+++ app/src/main/java/vn/tonish/hozo/activity/profile/ProfileActivity.java
@@ -290,7 +290,8 @@ public class ProfileActivity extends BaseActivity implements View.OnClickListene
 
             tvName.setText(mUserEntity.getFullName());
 
-            Utils.displayImage(this, imgBackground, mUserEntity.getBackground());
+            if (!TextUtils.isEmpty(mUserEntity.getBackground()))
+                Utils.displayImage(this, imgBackground, mUserEntity.getBackground());
 
             String genderAge = "";
 
